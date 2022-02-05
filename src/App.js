1605d7cb5423edfff5fe161c1852505369a4f1c5
Marcos <marcosraimondi1@gmail.com>
2022-02-05 19:25:56 -0300

    projects

--- src/App.js
+++ src/App.js
@@ -142,73 +142,83 @@ function App() {
           </div>
           <div className="row gx-0">
             <div className="col-lg-6">
-              <a className="portfolio-item" href="#!">
+              <a
+                className="portfolio-item"
+                href="http://agro-crm-project.herokuapp.com"
+              >
                 <div className="caption">
                   <div className="caption-content">
-                    <div className="h2">Stationary</div>
+                    <div className="h2">Agro CRM</div>
                     <p className="mb-0">
-                      A yellow pencil with envelopes on a clean, blue backdrop!
+                      A Customer Relationship Management app!
                     </p>
                   </div>
                 </div>
                 <img
                   className="img-fluid"
-                  src="assets/img/portfolio-1.jpg"
+                  src="assets/img/agro-project.png"
                   alt="..."
+                  width="700"
+                  height="467"
                 />
               </a>
             </div>
             <div className="col-lg-6">
-              <a className="portfolio-item" href="#!">
+              <a
+                className="portfolio-item"
+                href="https://turnosyagenda.herokuapp.com"
+              >
                 <div className="caption">
                   <div className="caption-content">
-                    <div className="h2">Ice Cream</div>
-                    <p className="mb-0">
-                      A dark blue background with a colored pencil, a clip, and
-                      a tiny ice cream cone!
-                    </p>
+                    <div className="h2">Turnero</div>
+                    <p className="mb-0">Booking services made easy!</p>
                   </div>
                 </div>
                 <img
                   className="img-fluid"
-                  src="assets/img/portfolio-2.jpg"
+                  src="assets/img/turnero.jpg"
                   alt="..."
+                  width="700"
+                  height="467"
                 />
               </a>
             </div>
             <div className="col-lg-6">
-              <a className="portfolio-item" href="#!">
+              <a className="portfolio-item" href="">
                 <div className="caption">
                   <div className="caption-content">
-                    <div className="h2">Strawberries</div>
-                    <p className="mb-0">
-                      Strawberries are such a tasty snack, especially with a
-                      little sugar on top!
-                    </p>
+                    <div className="h2">Coming Soon</div>
+                    <p className="mb-0">In process...</p>
                   </div>
                 </div>
                 <img
                   className="img-fluid"
-                  src="assets/img/portfolio-3.jpg"
+                  src="assets/img/coming-soon.jpg"
                   alt="..."
+                  width="700"
+                  height="467"
                 />
               </a>
             </div>
             <div className="col-lg-6">
-              <a className="portfolio-item" href="#!">
+              <a
+                className="portfolio-item"
+                href="https://github.com/marcosraimondi1"
+              >
                 <div className="caption">
                   <div className="caption-content">
-                    <div className="h2">Workspace</div>
+                    <div className="h2">GitHub</div>
                     <p className="mb-0">
-                      A yellow workspace with some scissors, pencils, and other
-                      objects.
+                      Check Out all my projects in my github page!
                     </p>
                   </div>
                 </div>
                 <img
                   className="img-fluid"
-                  src="assets/img/portfolio-4.jpg"
+                  src="assets/img/github.png"
                   alt="..."
+                  width="700"
+                  height="467"
                 />
               </a>
             </div>
